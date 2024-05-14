4cf23ea177afa21504f7c4df2e5a67d54de5c533
Vincent Chang <vincentchang402@gmail.com>
2024-05-14 12:35:23 +0800

    Install Ubuntu font

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import '@fontsource/ubuntu/300.css';
+import '@fontsource/ubuntu/400.css';
+import '@fontsource/ubuntu/500.css';
+import '@fontsource/ubuntu/700.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
