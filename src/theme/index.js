319d1f29c75fd8ce26cac40b1263a344ce21fd72
Vincent Chang <vincentchang402@gmail.com>
2024-05-17 11:45:44 +0800

    Customize AppBar, Toolbar and Button

--- src/theme/index.js
+++ src/theme/index.js
@@ -9,6 +9,80 @@ const theme = createTheme({
       paper: COLOR.BLACK_1B,
     },
   },
+  typography: {
+    fontFamily: 'Ubuntu',
+  },
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          boxShadow: 'none',
+          backgroundImage: 'none',
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...{
+            color: COLOR.BLACK_12,
+            backgroundColor: COLOR.WHITE,
+            border: `1px solid ${COLOR.WHITE}`,
+          textTransform: 'uppercase',
+            fontSize: 12,
+            fontWeight: 700,
+            lineHeight: 1,
+            padding: '13px 16px',
+            minWidth: 0,
+            '&:hover': {
+              color: COLOR.WHITE,
+              backgroundColor: COLOR.BLACK_12,
+            },
+          },
+          ...(ownerState.variant === 'contained' && {
+            fontFamily: 'Open Sans',
+            color: COLOR.WHITE,
+            backgroundColor: COLOR.BLACK_12,
+            borderRadius: 20,
+            textTransform: 'none',
+            fontSize: 12,
+            fontWeight: 600,
+            lineHeight: 1,
+            boxSizing: 'border-box',
+            padding: '8px 10px',
+            '&:hover': {
+              color: COLOR.BLACK_12,
+              backgroundColor: COLOR.WHITE,
+            },
+          }),
+          ...(ownerState.variant === 'outlined' && {
+            fontFamily: 'Open Sans',
+            color: COLOR.BLACK_12,
+            backgroundColor: COLOR.WHITE,
+            borderRadius: 20,
+            textTransform: 'none',
+            fontSize: 12,
+            fontWeight: 600,
+            lineHeight: 1,
+            boxSizing: 'border-box',
+            padding: '8px 10px',
+            '&:hover': {
+              color: COLOR.WHITE,
+              backgroundColor: COLOR.BLACK_12,
+              border: `1px solid ${COLOR.WHITE}`,
+            },
+          }),
+        }),
+      },
+    },
+    MuiToolbar: {
+      styleOverrides: {
+        root: {
+          height: 70,
+        },
+    },
+    },
+  },
 });
 
 export default theme;
