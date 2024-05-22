ad5133d7051545d07e5f97568cde0e529ace5138
Vincent Chang <vincentchang402@gmail.com>
2024-05-22 23:17:09 +0800

    Fix layout of NavBottom

--- src/components/NavBottom/index.js
+++ src/components/NavBottom/index.js
@@ -8,11 +8,14 @@ function NavBottom() {
   return (
     <BottomNavigation
       sx={{
+        height: 66,
         display: { sm: 'none' },
+        backgroundColor: '#18181833',
+        boxShadow: '0px 0.5px 0px 0px #000000CC inset',
       }}
     >
-      <BottomNavigationAction label="/home" component={Link} to="/home" icon={<HomeIcon />} />
-      <BottomNavigationAction label="/tags" component={Link} to="/tags" icon={<TagsIcon />} />
+      <BottomNavigationAction label="/home" component={Link} to="/home" icon={<HomeIcon isSelected />} sx={{ padding: '17px 12px 0 109px' }} />
+      <BottomNavigationAction label="/tags" component={Link} to="/tags" icon={<TagsIcon />} sx={{ padding: '17px 109px 0 12px' }} />
     </BottomNavigation>
   );
 }
