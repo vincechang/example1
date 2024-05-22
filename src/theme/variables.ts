56fc9c2b0e1d6936062846ec31e46a45a14be187
Vincent Chang <vincentchang402@gmail.com>
2024-05-22 14:30:57 +0800

    Add GREY_8A to color

--- src/theme/variables.ts
+++ src/theme/variables.ts
@@ -6,6 +6,7 @@ export enum COLOR {
   GREY_F8 = '#F8F8F8',
   GREY_B2 = '#B2B2B2',
   GREY_92 = '#929292',
+  GREY_8A = '#8A8A8A',
   GREY_6A = '#6A6A6A',
   BLACK_1F = '#1F1F1F',
   BLACK_1B = '#1B1B1B',
