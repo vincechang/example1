c978f53dbfa0aa96125cdcd301d4f820e0c6ae18
Vincent Chang <vincentchang402@gmail.com>
2024-05-23 20:41:44 +0800

    Fetch data from the followers and following APIs in the HomePage loader

--- src/routes/RootRouter.js
+++ src/routes/RootRouter.js
@@ -1,10 +1,13 @@
 import {
-  createBrowserRouter, Navigate,
+  createBrowserRouter, defer, Navigate,
 } from 'react-router-dom';
+import axios from 'axios';
 import NavLayout from 'pages/NavLayout';
 import HomePage from 'pages/HomePage';
 import ResultPage from 'pages/ResultPage';
 import TagsPage from 'pages/TagsPage';
+
+const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 const rootRouter = createBrowserRouter([
   {
@@ -12,7 +15,15 @@ const rootRouter = createBrowserRouter([
     element: <NavLayout />,
     children: [
       { index: true, element: <Navigate to="home" /> },
-      { path: 'home', element: <HomePage /> },
+      {
+        path: 'home',
+        element: <HomePage />,
+        loader: async () => {
+          const followers = await fetcher('https://avl-frontend-exam.herokuapp.com/api/users/all?page=1&pageSize=10');
+          const following = await fetcher('https://avl-frontend-exam.herokuapp.com/api/users/friends?page=1&pageSize=10');
+          return defer({ followers, following });
+        },
+      },
       { path: 'home/result/:query', element: <ResultPage /> },
       { path: 'tags', element: <TagsPage /> },
     ],
