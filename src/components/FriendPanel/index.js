fe272912730b7f0419e78e6c22b69d683ce2275f
Vincent Chang <vincentchang402@gmail.com>
2024-05-25 23:59:53 +0800

    Implement the infinite scroll feature for the FriendPanel

--- src/components/FriendPanel/index.js
+++ src/components/FriendPanel/index.js
@@ -1,36 +1,51 @@
 /* eslint-disable react/prop-types */
 import {
-  Avatar, Box, Button, List, ListItem, ListItemAvatar, ListItemText,
+  Avatar, Button, List, ListItem, ListItemAvatar, ListItemText,
 } from '@mui/material';
+import useSWRInfinite from 'swr/infinite';
 import { COLOR } from 'theme/variables.ts';
+import InfiniteScroll from 'components/InfiniteScroll';
+import fetcher from 'api';
 
 function FriendPanel({
-  value, index, friends: {
+  getApi, friends: {
     // eslint-disable-next-line no-unused-vars
-    data, page, pageSize, total, totalPage,
+    data: initialList, page: pageProp, pageSize, total, totalPages,
   },
 }) {
+  const {
+    data: fetchedData,
+    size: page,
+    setSize: setPage,
+    isValidating,
+  } = useSWRInfinite(
+    (p) => getApi(p + 1, pageSize),
+    fetcher,
+  );
+
+  const list = fetchedData ? fetchedData.flatMap(({ data }) => [...data]) : initialList;
+
   return (
-    <Box
-      role="tabpanel"
-      hidden={value !== index}
-      sx={{ px: '16px', py: '32px', overflowY: 'scroll' }}
-      height="calc(100vh - 66px)"
-    >
-        <List>
-          {data.map(({
-            id, name, username, avater, isFollowing,
-          }) => (
-            <ListItem key={id} sx={{ height: 45, padding: 0, mb: '16px' }}>
-              <ListItemAvatar>
-                <Avatar alt={username} src={avater} variant="rounded" sx={{ borderRadius: '5px', border: `1px solid ${COLOR.GREY_F8}` }} />
-              </ListItemAvatar>
-              <ListItemText primary={name} secondary={username} />
-              <Button variant={isFollowing ? 'outlined' : 'contained'}>{isFollowing ? 'Following' : 'Follow'}</Button>
-            </ListItem>
-          ))}
-        </List>
-    </Box>
+    <List sx={{ p: 0 }}>
+      <InfiniteScroll
+        onLoadMore={() => setPage(page + 1)}
+        isLoading={isValidating}
+        isEnd={page === totalPages}
+      >
+        {list.map(({
+          id, name, username, avater, isFollowing,
+        }) => (
+          <ListItem key={id} sx={{ height: 45, padding: 0, mb: '16px' }}>
+            <ListItemAvatar>
+              <Avatar alt={username} src={avater} variant="rounded" sx={{ borderRadius: '5px', border: `1px solid ${COLOR.GREY_F8}` }} />
+            </ListItemAvatar>
+            <ListItemText primary={name} secondary={username} />
+            <Button variant={isFollowing ? 'outlined' : 'contained'}>{isFollowing ? 'Following' : 'Follow'}</Button>
+          </ListItem>
+        ))}
+        {isValidating && (<div>Loading</div>)}
+      </InfiniteScroll>
+    </List>
   );
 }
 
