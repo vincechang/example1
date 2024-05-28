0aa1c35072bd0555774ad714c0e02bdb20c5e443
Vincent Chang <vincentchang402@gmail.com>
2024-05-28 23:13:23 +0800

    Extract NavItem

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,43 +1,8 @@
-import {
-  Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, Typography,
-} from '@mui/material';
-import { Link, useMatch } from 'react-router-dom';
+import { Box, Drawer, List } from '@mui/material';
 import { LogoIcon } from 'components/Icons';
 import { COLOR } from 'theme/variables.ts';
 import NavItems from 'configs/navItems';
-
-// eslint-disable-next-line react/prop-types
-function NavItem({ path, icon, label }) {
-  const isSelected = Boolean(useMatch(`${path}/*`));
-  return (
-    <ListItem
-      key={path}
-      disablePadding
-      sx={{
-        mb: '6px',
-        '&:hover .MuiTypography-root': {
-          color: isSelected ? COLOR.WHITE : COLOR.GREY_8A,
-          visibility: 'visible',
-        },
-      }}
-    >
-      <ListItemButton component={Link} to={path} display="flex" sx={{ flexDirection: 'column' }}>
-        <ListItemIcon>
-          {icon({ isSelected })}
-        </ListItemIcon>
-        <Typography
-          sx={{
-            fontSize: 12,
-            letterSpacing: 0.4,
-            visibility: isSelected ? 'visible' : 'hidden',
-          }}
-        >
-          {label}
-        </Typography>
-      </ListItemButton>
-    </ListItem>
-  );
-}
+import NavItem from 'components/NavBar/NavItem';
 
 // eslint-disable-next-line react/prop-types
 function NavBar({ width }) {
