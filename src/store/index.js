7ba2b455fed2324e1b18fcca37dd6534e2252590
Vincent Chang <vincentchang402@gmail.com>
2024-05-28 21:45:18 +0800

    Export vanilla store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
-import { create } from 'zustand';
+import { createStore } from 'zustand/vanilla';
+import { useStore as useStoreHook } from 'zustand';
 
-const useStore = create((set) => ({
+export const store = createStore((set) => ({
   keyword: '',
   setKeyword: (keyword) => set({ keyword }),
   pageSize: 0,
   setPageSize: (pageSize) => set({ pageSize }),
 }));
+
+const useStore = (selector) => useStoreHook(store, selector);
 
 export default useStore;
